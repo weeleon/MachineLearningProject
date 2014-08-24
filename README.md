b5b3d48ae69e69bf7238e37c9a8487f0db6d02c1
Leonard Wee <lw6945@gmail.com>
2014-08-24 08:47:34 +0200

    update training script

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
 MachineLearningProject
 ======================
 
-R code and markdown for Machine Learning Course Project
+R code and markdown for Practical Machine Learning Coursera module
+------------------------------------------------------------------
+This project is submitted to address the requirements of the above module, as
+per the assignment 2 project briefing.
+
+The essential steps in the included R script are as follows :
+1. Check for the presence of the required files in the defined data directory
+(line 12 of code) and, if absent, download it.
+2. The test data is examined only the first 2 lines to obtain the list of parameters
+(column names) which can be used as inputs to prediction (i.e. there is no use
+fitting a machine learning model that receives the value NA). The test data is
+deleted from R memory for all purposes of training and validation.
+3. The training data is internally pre-processed and cross-validated as 10-fold
+partitions.
+4.
+
+
+
+
+
+
+
+From the observations it can be seen that some of the available values are highly
+skewed with only one or two extreme values
+
+
+
+
+Citation
+========
+Velloso, E.; Bulling, A.; Gellersen, H.; Ugulino, W.; Fuks, H. Qualitative Activity
+Recognition of Weight Lifting Exercises. Proceedings of 4th International Conference
+in Cooperation with SIGCHI (Augmented Human '13) . Stuttgart, Germany: ACM SIGCHI, 2013.
+
+Read more: http://groupware.les.inf.puc-rio.br/har#weight_lifting_exercises#ixzz39tstDjrm
+
+
---
+++ project.R
@@ -0,0 +1,205 @@
+#project.R is a machine learning predictive script that classifies a set
+#of wearable device parameters as one of five classes A,B,C,D or E.
+#
+#
+
+#required packages
+library(caret)
+library(gbm)
+library(plyr)
+library(rpart)
+library(randomForest)
+
+#initialise path to data location
+dataDirectory <- "/Users/lenw/Documents/Coursera_Offline/Data_Science/Machine_Learning_Project"
+setwd(dataDirectory)
+
+#prepare to read training and testing data
+testData <- "pml-testing.csv" #relative path to testing data
+trainData <- "pml-training.csv" #relative path to training data
+
+#if the test and training machine learning dataset does not exist in dataDirectory
+#then download it
+if (!file.exists(trainData)) {
+      download.file("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv",trainData,method="curl")      
+      dateDownloaded <- paste(date(),"CEST (Copenhagen, Denmark)")
+      write.table(dateDownloaded,file="pml-training-download-date.txt",col.names=F,row.names=F,quote=F)
+      }
+
+if (!file.exists(testData)) {
+      download.file("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv",testData,method="curl")      
+      dateDownloaded <- paste(date(),"CEST (Copenhagen, Denmark)")
+      write.table(dateDownloaded,file="pml-testing-download-date.txt",col.names=F,row.names=F,quote=F)
+      }
+
+
+
+#load the training data set
+indata <- read.csv(trainData)
+#examine the distribution of data in the file before data splitting :
+TrainingSummaryTable <- with(indata, table(user_name,classe))
+#print the training data distribution on the console
+TrainingSummaryTable
+
+
+
+#only read the test data set to get the names of the usable columns for prediction
+testing <- read.csv(testData,nrows=2)
+
+#the final testing and evaluation can only be done on 60 non-NA columns
+#coumns with NA values can be used to predict "classe" in the final set
+#therefore there is no reason to build these into a prediction model
+usefulcolumns <- !is.na(testing[1,]) #what are the usable prediction parameters?
+indata <- indata[,usefulcolumns] #only select these as prediction parameters
+
+#likewise, we do not want to allow machine learning to construct a prediction
+#from time stamps, or user name, or window numbers, so drop these as well
+indata <- indata[,c(8:60)] #but exclude the identifier data in the first seven columns
+
+#remove the testing data so there is no way to accidentally visualise it
+rm(testing)
+rm(usefulcolumns)
+
+
+
+#check data for zero variance parameters
+nzv <- nearZeroVar(indata[,-53], saveMetrics = TRUE)
+if (sum(nzv$nzv) > 0) { print("Warning - there are parameters with zero or near-zero variance.")}
+rm(nzv)
+#- a zero sum implies that none of the 52 parameters are zero or near-zero variance
+
+#NB : There are no zero-variance parameters among the available 52 columns
+
+
+#check for tightly correlated parameters which would be redundant for prediction
+#we arbitrarily set a correlation absolute threshold of 0.90
+#filter and name the correlated variables
+highlyCor <- findCorrelation(cor(indata[,-53]), cutoff = 0.90)
+names(indata)[highlyCor]
+#remove variables that are highly correlated with other variables
+filteredInData <- indata[,-highlyCor]
+
+#NB : This pre-processing step has removed 7 variables from the column list
+#resulting in 45 parameters available for machine-based prediction
+
+
+
+#generate a generic command sequence for training control for any learning
+fitControl <- trainControl(
+      ## 10-fold repeated cross-validation
+      method = "repeatedcv", number = 10,
+      ## repeated five times
+      repeats = 5
+      ## no pre-processing scaling and centering not critical for tree methods
+      )
+
+
+
+# ----------- create calls to the training object
+
+## learning object 1 : simple classification tree with all variables
+set.seed(33524) #set initial seed
+treeFit1 <- train(classe ~ ., data = indata, method = "rpart",
+                  trControl = fitControl,
+                  tuneGrid = expand.grid(.cp=c(0.0001,0.001,0.003,0.01,0.03,0.1))
+                  )
+
+## learning object 2 : simple classification tree with correlation-filtered variables
+set.seed(33524) #set initial seed
+treeFit2 <- train(classe ~ ., data = filteredInData, method = "rpart",
+                  trControl = fitControl,
+                  tuneGrid = expand.grid(.cp=c(0.0001,0.001,0.003,0.01,0.03,0.1))
+)
+## this simple object sets out the baseline accuracy at roughly 83% for a
+## complexity parameter of 0.003
+
+
+
+## learning object 3 : random forest classification with all variables
+set.seed(33524) #reset initial seed
+forestFit1 <- train(classe ~ ., data = indata, method = "rf",
+                    trControl = fitControl,
+                    allowParallel = TRUE,
+                    tuneGrid = expand.grid(.mtry=c(23,27,45,52))
+                    )
+
+
+## learning object 4 : random forest classification with correlation-filtered variables
+set.seed(33524) #reset initial seed
+forestFit2 <- train(classe ~ ., data = filteredInData, method = "rf",
+                    trControl = fitControl,
+                    allowParallel = TRUE,
+                    tuneGrid = expand.grid(.mtry=c(23,27,45))
+                    )
+
+## for parallel processing ...
+library(doMC)
+registerDoMC(cores=2)
+fitControl <- trainControl(
+      ## 10-fold repeated cross-validation
+      method = "repeatedcv", number = 10,
+      ## repeated five times
+      repeats = 5,
+      ## no pre-processing scaling and centering not critical for tree methods
+      allowParallel = TRUE
+)
+
+## learning object 5 : gradient boosting with all variables
+set.seed(33524) #set initial seed
+gbmFit1 <- train(classe ~ ., data = indata, method = "gbm",
+                 trControl = fitControl,
+                 ##switch off verbose
+                 verbose = FALSE
+                 )
+
+## learning object 6 : gradient boosting with correlation-filtered variables
+set.seed(33524) #set initial seed
+gbmFit2 <- train(classe ~ ., data = filteredInData, method = "gbm",
+                 trControl = fitControl,
+                 ##switch off verbose
+                 verbose = FALSE
+                 )
+
+
+
+# ----------- apply the best model with the best tuning parameter
+# to the whole training set
+testing <- read.csv(testData)
+usefulcolumns <- !is.na(testing[1,]) #what are the usable prediction parameters?
+finalTest <- testing[,usefulcolumns] #only select these as prediction parameters
+finalTest <- finalTest[,c(8:60)] #but exclude the identifier data in the first seven columns
+
+treeFit1
+## maximal accuracy 94% with 10-fold cross-validation in 5 reps
+
+treeFit2
+## maximal accuracy 93.5% with 10-fold cross-validation in 5 reps
+## but faster run on reduced parameter set (removed close correlations)
+
+forestFit1
+## maximal accuracy 99.6% with 10-fold cross-validation in 5 reps
+
+forestFit2
+## maximal accuracy 99.5% with 10-fold cross-validation in 5 reps
+## but faster run on reduced parameter set (removed close correlations)
+
+## choose forestFit2 for final prediction
+answer <- predict(forestFit2, newdata=finalTest)
+answer
+## submission of this answer yields 20/20 correct on the final test set
+
+
+## prepare to write answer file
+pml_write_files = function(x){
+      n = length(x)
+      for(i in 1:n){
+            filename = paste0("problem_id_",i,".txt")
+            write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+      }
+}
+
+## export answer files
+pml_write_files(answer)
+
+
+
